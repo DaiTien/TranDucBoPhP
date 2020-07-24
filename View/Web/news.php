9b5408c4afe8a023c7c525cb13970b9cf6329178
Quoc Luong <luongd872@gmail.com>
2020-07-24 13:53:37 +0700

    Luong fix new and productdetails

--- View/Web/news.php
+++ View/Web/news.php
@@ -39,7 +39,7 @@
                 <section class="breadcrumb-part" data-stellar-offset-parent="true" data-stellar-background-ratio="0.5" style="background-image: url('/TranDucBoPhP/asset/Web/images/breadbg1.jpg');">
                     <div class="container">
                         <div class="breadcrumb-inner">
-                            <h2>Trang Chủ</h2>
+                            <h2>Tin Tức</h2>
                             <a href="#">Trang Chủ</a>
                             <span>Tin Tức</span>
                         </div>
@@ -57,44 +57,22 @@
                                 <div class="blog-right-section">
                                     <div class="blog-right-listing wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
                                         <div class="feature-img">
-                                            <img src="/TranDucBoPhP/asset/Web/images/img49.png" alt="">
-                                            <div class="date-feature">27
-                                                <br> <small>may</small></div>
+                                            <img src="/TranDucBoPhP/asset/Web/images/lt-okinawa.png" alt="">
+                                            <!-- <div class="date-feature">27
+                                                <br> <small>may</small></div> -->
                                         </div>
-                                        <div class="feature-info">
-                                            <span><i class="icon-user-1"></i> By Ali TUFAN</span>
-                                            <span><i class="icon-comment-5"></i> 5 Comments</span>
-                                            <h5>How Do You Like Your Sausage?</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget viverra nulla sem vitae neque. Quisque id sodales libero. In nec enim nisi, in ultricies quam. Sed lacinia feugiat velit, cursus molestie lectus.</p>
-                                            <a href="blog_single.html" class="button-default">Read More <i class="icon-right-4"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="blog-right-listing wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
-                                        <div class="feature-img">
-                                            <img src="/TranDucBoPhP/asset/Web/images/img50.png" alt="">
-                                            <div class="date-feature">27
-                                                <br> <small>may</small></div>
-                                        </div>
-                                        <div class="feature-info">
-                                            <span><i class="icon-user-1"></i> By Ali TUFAN</span>
-                                            <span><i class="icon-comment-5"></i> 5 Comments</span>
-                                            <h5>There are many variations of passages</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget viverra nulla sem vitae neque. Quisque id sodales libero. In nec enim nisi, in ultricies quam. Sed lacinia feugiat velit, cursus molestie lectus.</p>
-                                            <a href="blog_single.html" class="button-default">Read More <i class="icon-right-4"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="blog-right-listing wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
-                                        <div class="feature-img">
-                                            <img src="/TranDucBoPhP/asset/Web/images/img51.png" alt="">
-                                            <div class="date-feature">27
-                                                <br> <small>may</small></div>
-                                        </div>
-                                        <div class="feature-info">
-                                            <span><i class="icon-user-1"></i> By Ali TUFAN</span>
-                                            <span><i class="icon-comment-5"></i> 5 Comments</span>
-                                            <h5>There are many variations of passages</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget viverra nulla sem vitae neque. Quisque id sodales libero. In nec enim nisi, in ultricies quam. Sed lacinia feugiat velit, cursus molestie lectus.</p>
-                                            <a href="blog_single.html" class="button-default">Read More <i class="icon-right-4"></i></a>
+                                        <div class="feature-info feature-info--modify">
+                                            <!-- <span><i class="icon-user-1"></i> By Ali TUFAN</span>
+                                            <span><i class="icon-comment-5"></i> 5 Comments</span> -->
+                                            <h5>Series Long Nhãn Táo Đỏ đã trở lại trong hè này</h5>
+                                            <p>Các Fan BoBo MilkTea ơi! Món thức uống chào hè đã quay trở lại sau 1 năm xa cách đây! Nguyên liệu từ nhãn nhục và táo đỏ được Gong Cha kết hợp khéo léo với trà nguyên chất và sữa tươi để mang trở lại 3 siêu phẩm mà các Fan yêu thích hè năm ngoái nhưng chưa kịp thưởng thức vì nhanh chóng “sold out”.
+                                                Tất nhiên ưu đãi dòng sản phẩm chào hè đồng giá 💸39.000đ (M) và 💸45.000đ (L) dành cho Fan trong tuần đầu “come back” là không thể thiếu rồi!
+                                                📌 Lưu ý:
+                                                - Chương trình áp dụng khi mua trực tiếp tại cửa hàng hoặc đặt hàng qua hotline giao hàng của cửa hàng
+                                                - Không áp dụng chung với các CTKM khác
+                                                - Không áp dụng giảm thêm với thành viên của Gong Cha E-members
+                                                - Không áp dụng giá ưu đãi trên kênh giao hàng Now, GrabFood, Baemin…</p>
+                                            <!-- <a href="blog_single.html" class="button-default">Read More <i class="icon-right-4"></i></a> -->
                                         </div>
                                     </div>
                                     <div class="gallery-pagination">
