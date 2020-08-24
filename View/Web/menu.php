b76c4ac7a6f1fe59b80987cd3caac525895ce17e
Quoc Luong <luongd872@gmail.com>
2020-08-24 17:22:05 +0700

    Luong fix menu mobile chua duoc

--- View/Web/menu.php
+++ View/Web/menu.php
@@ -1,5 +1,5 @@
 <style>
-    #btnLogout {
+    #btnLogout{
         border: 1px solid white;
         display: block;
         width: 73px;
@@ -13,34 +13,38 @@
     }
 </style>
 <div class="header-bottom">
-    <div class="container">
-        <div class="header-info">
-            <div class="header-info-inner menu-main">
-                <ul>
-                    <li class="has-child">
-                        <a class="menu__link" href="#gallery">Thư Viện Ảnh</a>
+                    <div class="container">
+                        <div class="header-info">
+                            <div class="header-info-inner menu-main">
+                                <ul>
+                                    <li class="has-child">
+                                        <a class="menu__link" href="#gallery">Thư Viện Ảnh</a>
 
-                    </li>
-                    <li class="has-child">
-                        <a class="menu__link" id="lienhe" href="#contract">Liên Hệ</a>
-                    </li>
-                    <li class="has-child">
-                        <?php
+                                    </li>
+                                    <li class="has-child">
+                                        <a class="menu__link" id="lienhe" href="#contract">Liên Hệ</a>
+                                    </li>
+                                    <li class="has-child">
+                                        <?php
 
-                        if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
-                            echo '<a >Xin chào ' . $user . '</a>';
-                            echo '<a href="?c=IndexWebsite&a=Logout" id="btnLogout" class="btn">Logout</a>';
-                            echo '<ul class="drop-nav">';
-                            echo '<li><a href="?c=IndexWebsite&a=Order">Giỏ hàng</a></li>';
-                            echo '<li><a href="#">Sản phẩm yêu thích</a></li>';
-                            echo '</ul>';
-                        } else {
-                            echo "<a id=\"login_register\" href=\"#login\">Đăng Nhập&nbsp;/&nbsp;Đăng Ký</a>";
-                        }
-                        ?>
-                    </li>
-                </ul>
-                <!--<div class="search-part">
+                                        if (isset($_SESSION['login']) && $_SESSION['login'] == 1)
+                                        {
+                                            echo '<a >Xin chào ' .$user .'</a>';
+                                            echo '<a href="?c=IndexWebsite&a=Logout" id="btnLogout" class="btn">Logout</a>';
+                                            echo '<ul class="drop-nav">';
+                                            echo '<li><a href="?c=IndexWebsite&a=Order">Giỏ hàng</a></li>';
+                                            echo '<li><a href="#">Sản phẩm yêu thích</a></li>';
+                                            echo '</ul>';
+
+                                        }
+                                        else
+                                        {
+                                            echo "<a id=\"login_register\" href=\"#login\">Đăng Nhập&nbsp;/&nbsp;Đăng Ký</a>";
+                                        }
+                                        ?>
+                                    </li>
+                                </ul>
+                                <!--<div class="search-part">
                                     <a class="search-part" href="#"></a>
                                     <div class="search-box">
                                         <input type="text" name="txt" placeholder="Search">
@@ -48,40 +52,39 @@
                                     </div>
                                 </div>-->
 
-            </div>
-        </div>
-        <div class="menu-icon">
-            <a href="#" class="hambarger">
-                <span class="bar-1"></span>
-                <span class="bar-2"></span>
-                <span class="bar-3"></span>
-            </a>
-        </div>
-        <div class="book-table header-collect book-sm">
-            <a href="#" data-toggle="modal" data-target="#booktable"><img src="asset/web/images/icon-table.png" alt="">Book a Table</a>
-        </div>
-        <div class="menu-main">
-            <ul>
-                <li class="has-child">
-                    <a href="?c=IndexWebsite">Trang Chủ</a>
-                </li>
-                <li class="mega-menu">
-                    <a class="menu__link" id="san_pham" href="#products">Sản Phẩm</a>
-
-                </li>
-                <li class="has-child">
-                    <a class="menu__link" href="#reach-to">Giới Thiệu</a>
-
-                </li>
-                <li class="has-child">
-                    <a onclick="closeFunction()" class="menu__link" href="#news">Tin Tức</a>
-
-                </li>
-            </ul>
-        </div>
-        <div class="logo">
-            <a href="index.php"><img src="asset/web/images/logo.png" alt=""></a>
-        </div>
-    </div>
-</div>
-
+                            </div>
+                        </div>
+                        <div class="menu-icon">
+                            <a href="#" class="hambarger">
+                                <span class="bar-1"></span>
+                                <span class="bar-2"></span>
+                                <span class="bar-3"></span>
+                            </a>
+                        </div>
+                        <div class="book-table header-collect book-sm">
+                            <a href="#" data-toggle="modal" data-target="#booktable"><img src="asset/web/images/icon-table.png" alt="">Book a Table</a>
+                        </div>
+                        <div class="menu-main">
+                            <ul>
+                                <li class="has-child">
+                                    <a href="?c=IndexWebsite">Trang Chủ</a>
+                                </li>
+                                <li class="mega-menu">
+                                    <a class="menu__link" id="san_pham" href="#products">Sản Phẩm</a>
+                                    
+                                </li>
+                                <li class="has-child">
+                                    <a class="menu__link" href="#reach-to">Giới Thiệu</a>
+        
+                                </li>
+                                <li class="has-child">
+                                    <a class="menu__link" href="#news">Tin Tức</a>
+                                 
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="logo">
+                            <a href="index.php"><img src="asset/web/images/logo.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
