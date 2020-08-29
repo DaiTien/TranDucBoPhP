75296f92393918bb58566ab3b9e1c14a0817d366
Quoc Luong <luongd872@gmail.com>
2020-08-29 15:03:48 +0700

    Luong fix admin

--- View/Admin/dashboard.php
+++ View/Admin/dashboard.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,146 +27,149 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
     <style>
-        .custom-banner{
+        .custom-banner {
             max-height: 400px;
             overflow: hidden;
             text-align: center;
         }
-        .custom-banner a{
+
+        .custom-banner a {
             position: relative;
             top: 200px;
             z-index: 1;
         }
     </style>
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-    <!--Header -->
-    <?php
-    include 'header.php';
-    ?>
-    <!-- Left side column. contains the logo and sidebar -->
-    <?php
-    include 'menu.php';
-    ?>
+        <!--Header -->
+        <?php
+        include 'header.php';
+        ?>
+        <!-- Left side column. contains the logo and sidebar -->
+        <?php
+        include 'menu.php';
+        ?>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
 
-        <!-- CONTEN HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-        <section class="content">
-            <div class="container-fluid">
-                <!-- Small boxes (Stat box) -->
-                <div class="row">
-                    <div class="col-lg-3 col-6">
-                        <!-- small box -->
-                        <div class="small-box bg-info">
-                            <div class="inner">
-                                <h3>150</h3>
+            <!-- CONTEN HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Small boxes (Stat box) -->
+                    <div class="row">
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>150</h3>
 
-                                <p>SỐ LƯỢNG LƯỢT TRUY CẬP</p>
+                                    <p>SỐ LƯỢNG LƯỢT TRUY CẬP</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-bag"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">.</a>
                             </div>
-                            <div class="icon">
-                                <i class="ion ion-bag"></i>
-                            </div>
-                            <a href="#" class="small-box-footer">.</a>
                         </div>
-                    </div>
-                    <!-- ./col -->
-                    <div class="col-lg-3 col-6">
-                        <!-- small box -->
-                        <div class="small-box bg-success">
-                            <div class="inner">
-                                <h3><?php echo $totalProduct?></h3>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <h3><?php echo $totalProduct ?></h3>
 
-                                <p>TỔNG SẢN PHẨM</p>
+                                    <p>TỔNG SẢN PHẨM</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-stats-bars"></i>
+                                </div>
+                                <a href="?c=Product&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
-                            <div class="icon">
-                                <i class="ion ion-stats-bars"></i>
-                            </div>
-                            <a href="?c=Product&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
-                    </div>
-                    <!-- ./col -->
-                    <div class="col-lg-3 col-6">
-                        <!-- small box -->
-                        <div class="small-box bg-warning">
-                            <div class="inner">
-                                <h3><?php echo $totalMember ?></h3>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3><?php echo $totalMember ?></h3>
 
-                                <p>TỔNG THÀNH VIÊN</p>
+                                    <p>TỔNG THÀNH VIÊN</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="?c=UserAdmin&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
-                            <div class="icon">
-                                <i class="ion ion-person-add"></i>
-                            </div>
-                            <a href="?c=UserAdmin&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
-                    </div>
-                    <!-- ./col -->
-                    <div class="col-lg-3 col-6">
-                        <!-- small box -->
-                        <div class="small-box bg-danger">
-                            <div class="inner">
-                                <h3><?php echo $totalFeedBack ?></h3>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-danger">
+                                <div class="inner">
+                                    <h3><?php echo $totalFeedBack ?></h3>
 
-                                <p>PHẢN HỒI KHÁCH HÀNG</p>
+                                    <p>PHẢN HỒI KHÁCH HÀNG</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-pie-graph"></i>
+                                </div>
+                                <a href="?c=FeedBackAdmin&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
-                            <div class="icon">
-                                <i class="ion ion-pie-graph"></i>
-                            </div>
-                            <a href="?c=FeedBackAdmin&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
+                        <!-- ./col -->
                     </div>
-                    <!-- ./col -->
-                </div>
-                <div class="row">
-                    <div class="custom-banner col-sm-12">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/X7rjPgr8xhY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <!--<a href="order" class="btn btn-success btn-lg">BẮT ĐẦU</a>
+                    <div class="row">
+                        <div class="custom-banner col-sm-12">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/NokwU_ZsF-I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <!--<a href="order" class="btn btn-success btn-lg">BẮT ĐẦU</a>
                         <img src="asset/admin/admin_images/banner.jpg" style="width: 100%; filter: brightness(.6);">-->
+                        </div>
                     </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
-        <!-- ~END CONTENT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   -->
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- ~END CONTENT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   -->
+        </div>
+        <!-- /.content-wrapper -->
+        <?php
+        include 'footer.php';
+        ?>
     </div>
-    <!-- /.content-wrapper -->
-    <?php
-    include 'footer.php';
-    ?>
-</div>
-<!-- jQuery -->
-<script src="asset/admin/AdminLTE/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="asset/admin/AdminLTE/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="asset/admin/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="asset/admin/AdminLTE/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="asset/admin/AdminLTE/plugins/sparklines/sparkline.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="asset/admin/AdminLTE/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="asset/admin/AdminLTE/plugins/moment/moment.min.js"></script>
-<script src="asset/admin/AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="asset/admin/AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="asset/admin/AdminLTE/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="asset/admin/AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="asset/admin/AdminLTE/dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="asset/admin/AdminLTE/dist/js/demo.js"></script>
+    <!-- jQuery -->
+    <script src="asset/admin/AdminLTE/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="asset/admin/AdminLTE/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="asset/admin/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- ChartJS -->
+    <script src="asset/admin/AdminLTE/plugins/chart.js/Chart.min.js"></script>
+    <!-- Sparkline -->
+    <script src="asset/admin/AdminLTE/plugins/sparklines/sparkline.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="asset/admin/AdminLTE/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="asset/admin/AdminLTE/plugins/moment/moment.min.js"></script>
+    <script src="asset/admin/AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="asset/admin/AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Summernote -->
+    <script src="asset/admin/AdminLTE/plugins/summernote/summernote-bs4.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="asset/admin/AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="asset/admin/AdminLTE/dist/js/adminlte.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="asset/admin/AdminLTE/dist/js/demo.js"></script>
 </body>
+
 </html>
 
 <script>
