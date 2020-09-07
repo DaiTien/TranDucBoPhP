93b5909f00043a177de42df7743eab9efca66d28
Quoc Luong <luongd872@gmail.com>
2020-09-07 10:27:16 +0700

    Luong add popup

--- View/Web/login.php
+++ View/Web/login.php
@@ -14,10 +14,8 @@
                         <h3 class="text-coffee">Đăng Nhập</h3>
                         <span class="text-uppercase text-danger">
                             <?php
-                            if (isset($_GET['lg']))
-                            {
-                                if ($_GET['lg'] == 0)
-                                {
+                            if (isset($_GET['lg'])) {
+                                if ($_GET['lg'] == 0) {
                                     echo 'Tài khoản hoặc mật khẩu không đúng!';
                                 }
                             }
@@ -38,7 +36,7 @@
 
                                     </div>
                                     <div class="col-md-6 col-sm-6 col-xs-12">
-                                        <!--<a href="#" class="pull-right">Quên mật khẩu</a>-->
+                                        <a href="#" class="pull-right" data-toggle="modal" data-target="#booktable">Quên mật khẩu</a>
                                     </div>
                                 </div>
                             </div>
@@ -51,31 +49,53 @@
                         <hr>
                         <span>Or</span>
                     </div>
-<!--                    <div class="row">-->
-<!--                        <div class="col-md-6 col-sm-12 col-xs-12">-->
-<!--                            <a href="#" class="facebook-btn btn-change button-default"><i class="fa fa-facebook"></i>Đăng Nhập Facebook</a>-->
-<!--                        </div>-->
-<!--                        <div class="col-md-6 col-sm-12 col-xs-12">-->
-<!--                            <a href="#" class="tweeter-btn btn-change button-default"><i class="fa fa-twitter"></i>Đăng Nhập Twitter</a>-->
-<!--                        </div>-->
-<!--                    </div>-->
+                    <!--                    <div class="row">-->
+                    <!--                        <div class="col-md-6 col-sm-12 col-xs-12">-->
+                    <!--                            <a href="#" class="facebook-btn btn-change button-default"><i class="fa fa-facebook"></i>Đăng Nhập Facebook</a>-->
+                    <!--                        </div>-->
+                    <!--                        <div class="col-md-6 col-sm-12 col-xs-12">-->
+                    <!--                            <a href="#" class="tweeter-btn btn-change button-default"><i class="fa fa-twitter"></i>Đăng Nhập Twitter</a>-->
+                    <!--                        </div>-->
+                    <!--                    </div>-->
                 </div>
             </div>
+            <!-- Start Book Table -->
+            <div class="modal fade booktable" id="booktable" tabindex="-1" role="dialog" aria-labelledby="booktable">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <div class="table-title">
+                                <h2>Khôi Phục Mật Khẩu</h2>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <input type="text" name="user" placeholder="User name">
+                                </div>
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                <input type="email" name="email" placeholder="Email Address">
+                                </div>
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <button class="btn-main btn-big">Khôi phục mật khẩu</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Book Table -->
             <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
                 <div class="register-wrap form-common">
                     <div class="title text-center">
                         <h3 class="text-coffee">Đăng Ký</h3>
                         <span class="text-uppercase text-danger">
                             <?php
-                            if (isset($_GET['g']))
-                            {
-                                if ($_GET['g'] == 1)
-                                {
+                            if (isset($_GET['g'])) {
+                                if ($_GET['g'] == 1) {
                                     echo 'Đăng ký tài khoản thành công!';
-                                }else if ($_GET['g'] == 2)
-                                {
+                                } else if ($_GET['g'] == 2) {
                                     echo 'Xác nhận mật khẩu không chính xác!';
-                                }else{
+                                } else {
                                     echo 'Email or Tên đăng nhập đã tồn tại!';
                                 }
                             }
